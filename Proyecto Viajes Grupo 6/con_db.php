90a80e32907706eb16269c373d05b31f5036d952
JordiCervera <a17jorcermar@inspedralbes.cat>
2020-12-13 22:40:00 +0100

    intento de mostrar experiencias

--- Proyecto Viajes Grupo 6/con_db.php
+++ Proyecto Viajes Grupo 6/con_db.php
@@ -1,6 +1,6 @@
 <?php
 
-$connex = mysqli_connect("localhost","a19alvsildur_bd","123456789","a19alvsildur_viajes");
+$conex = mysqli_connect("localhost","a19alvsildur_bd","123456789","a19alvsildur_viajes");
 
 
 
