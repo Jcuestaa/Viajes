dafa6a95470f928be3d379aa4000f6ad69c8f0e9
Jcuestaa <73338489+Jcuestaa@users.noreply.github.com>
2020-12-15 10:44:12 +0100

    index.php

--- Proyecto Viajes Grupo 6/index.php
+++ Proyecto Viajes Grupo 6/index.php
@@ -42,19 +42,16 @@
         <div class="container d-flex align-items-center">
 
             <div class="logo mr-auto">
-                <h1 class="text-light"><a href="index.html"><span>Viajes-EXP</span></a></h1>
+                <h1 class="text-light"><a href="index.html"><img id="logo" src="assets/img/logo.png"></img></a></h1>
                 <!-- Uncomment below if you prefer to use an image logo -->
                 <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
             </div>
 
             <nav class="nav-menu d-none d-lg-block">
                 <ul>
-                    <li class="active"><a href="index.html">Página Inicial</a></li>
+                    <li class="active"><a href="index.html">Home</a></li>
                     <li><a href="#contact">Contáctanos</a></li>
-                    <li>
-                        <button type="button" id="inicarsessio">Login</button>
-
-                    </li>
+                    <li><a id="login">Login</a></li>
                 </ul>
             </nav><!-- .nav-menu -->
 
