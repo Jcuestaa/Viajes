2dc1aa76ff27ab4e63a11e2c99267fec143bb923
Jcuestaa <73338489+Jcuestaa@users.noreply.github.com>
2021-01-17 22:42:05 +0100

    Actualitzat

--- Proyecto Viajes Grupo 6/assets/js/popup.js
+++ Proyecto Viajes Grupo 6/assets/js/popup.js
@@ -1,8 +1,22 @@
 window.onload=function(){
-  let btnAbrirPopup = document.getElementById("login");
+  let btnAbrirPopup= document.getElementById("login");
   let overlay = document.getElementById("overlay");
   let popup = document.getElementById("popup");
   let btncerrarPopup = document.getElementById("btn-cerrar-popup");
+
+  let btnAbrirPopup2 = document.getElementById("registrar");
+  let overlay2 = document.getElementById("overlay2");
+  let popup2 = document.getElementById("popup2");
+  let btncerrarPopup2 = document.getElementById("btn-cerrar-popup2");
+
+  btnAbrirPopup2.addEventListener('click', function(){
+      overlay2.classList.add('active');
+      popup2.classList.add('active');
+  });
+  btncerrarPopup2.addEventListener('click', function(){
+      overlay2.classList.remove('active');
+      popup2.classList.remove('active');
+  });
 
   btnAbrirPopup.addEventListener('click', function(){
       overlay.classList.add('active');
@@ -14,3 +28,4 @@ window.onload=function(){
   });
 
 };
+
