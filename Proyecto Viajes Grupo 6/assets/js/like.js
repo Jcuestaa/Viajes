34b6085b1d9a4a8bb11708ca2a9f1a8ee4661224
Jcuestaa <73338489+Jcuestaa@users.noreply.github.com>
2021-01-17 23:58:17 +0100

    like.js

---
+++ Proyecto Viajes Grupo 6/assets/js/like.js
@@ -0,0 +1,24 @@
+window.onload= function(){
+    var boton = document.getElementsByClassName('like');
+    boton[0].addEventListener('click', function () {
+
+        axios.get('./php/like.php', {
+                params: {
+                    id: document.getElementById('1').value
+
+                }
+            })
+            .then(function (res) {
+                if (res.status == 201) {
+
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+            .then(function () {
+
+            })
+
+    });
+}
